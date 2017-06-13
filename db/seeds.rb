a11b72c9007ad88a64da45958387eecd123ad42f
Chris Yoo <woyoo@umich.edu>
2017-06-14 04:45:33 +0900

    finish backend-assignment-17

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,9 @@ posts = Post.all
   )
 end
 
+<Post id: 101, title: "The unique title", body: "The unique body", created_at: "2017-06-13 19:22:01", updated_at: "2017-06-13 19:22:01">
+<Comment id: 301, body: "The unique body for comment", post_id: 101, created_at: "2017-06-13 19:35:22", updated_at: "2017-06-13 19:35:22"> 
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
