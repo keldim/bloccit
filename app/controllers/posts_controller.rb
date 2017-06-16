7254e329b3d4a69ff4da69833c551414804ae49f
Chris Yoo <woyoo@umich.edu>
2017-06-17 03:12:30 +0900

    finish backend-assignment-18

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,14 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    @posts.each do |post|
+      if (post.id) == 1
+        post.title = "SPAM"
+      elsif (post.id)%5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
