83aba4971d9b9f464a5c2a6f4a28fd0c83522964
Chris Yoo <woyoo@umich.edu>
2017-07-12 21:45:25 -0400

    finish backend-checkpoint-28

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,11 +39,18 @@ posts = Post.all
 end
 
 
-user = User.first
- user.update_attributes!(
-   email: 'wonseok1992@naver.com', # replace this with your personal email
-   password: 'helloworld'
- )
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
 
 
 puts "Seed finished"
