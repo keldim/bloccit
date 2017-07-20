c3d71b39f10f3a0b11cc471c1c5049ea868b4a9f
Chris Yoo <woyoo@umich.edu>
2017-07-20 13:36:07 -0400

    finish backend-checkpoint-30

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
          resources :comments, only: [:create, :destroy]
        end
 
+       post '/up-vote' => 'votes#up_vote', as: :up_vote
+           post '/down-vote' => 'votes#down_vote', as: :down_vote
 
     resources :users, only: [:new, :create]
 
