d36bdbde69fbb2558d277d21248299517a79c2b2
Chris Yoo <woyoo@umich.edu>
2017-07-30 16:05:13 -0400

    finish backend-assginment-32

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+  def contentCheck
+    if @user.posts == nil || @user.comments == nil
+      "{user.name} has not submitted any posts or comments yet."
+    end
+
+  end
+
+end
+
 end
